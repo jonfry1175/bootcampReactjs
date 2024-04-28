fa99d312a9ff6388209971589d6f61416b22258c
jonfry1175 <odiboen@gmail.com>
2024-04-29 00:10:54 +0700

    styling using component NextUI & TailwindCSS

--- fundamental/src/App.jsx
+++ fundamental/src/App.jsx
@@ -2,7 +2,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import TodoCard from "./components/TodoCard";
 
-
 function App() {
   return (
     <>
@@ -11,12 +10,10 @@ function App() {
       <TodoCard />
       <TodoCard />
       <TodoCard />
-      <TodoCard />
-      <TodoCard />
-      <TodoCard />
       <Footer />
     </>
   );
 }
+
 
 export default App;
--- fundamental/src/components/TodoCard.jsx
+++ fundamental/src/components/TodoCard.jsx
@@ -1,16 +1,44 @@
-import React from 'react'
+import React from "react";
+
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Divider,
+} from "@nextui-org/react";
 
 const TodoCard = () => {
+  // return (
+  //   <div>
+  //       <h5>Monday</h5>
+  //       <ul>
+  //           <li>Sarapan</li>
+  //           <li>Makan</li>
+  //           <li>Tidur</li>
+  //       </ul>
+  //   </div>
+  // )
+
   return (
-    <div>
-        <h5>Monday</h5>
-        <ul>
+    <>
+      <Card className="max-w-[400px]">
+        <CardHeader className="font-bold text-lg">Monday</CardHeader>
+        <Divider />
+        <CardBody>
+          <ul className="list-decimal list list-inside">
             <li>Sarapan</li>
             <li>Makan</li>
             <li>Tidur</li>
-        </ul>
-    </div>
-  )
-}
+          </ul>
+        </CardBody>
+        <CardFooter>
+          <Button color="danger">Finish</Button>
+        </CardFooter>
+      </Card>
+    </>
+  );
+};
 
-export default TodoCard
+export default TodoCard;
