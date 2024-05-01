d02b49152a53b7eec1330da2694ae02e55bf55e7
jonfry1175 <odiboen@gmail.com>
2024-05-02 02:51:45 +0700

    HOC using functional component

--- fundamental/src/hoc/redBackground.jsx
+++ fundamental/src/hoc/redBackground.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
+// HOC function component
 export const redBackground = (InnerComponent) => {
-  class WrapperComponent extends React.Component {
-    render() {
-      return (
-        <div className="bg-red-500">
-          <InnerComponent {...this.props} />
-        </div>
-      );
-    }
-  }
+  const WrapperComponent = (props) => {
+    return (
+      <div className="bg-red-500">
+        <InnerComponent {...props} />
+      </div>
+    );
+  };
+
   return WrapperComponent;
 };
